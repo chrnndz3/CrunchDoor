5a017ef54151a77ccfd1d6ac2c4ececb5a895a8a
Jacob Waterman <jacobrwaterman@gmail.com>
2015-12-03 15:40:11 -0600

    hello

--- crunchDoorApp/views.py
+++ crunchDoorApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-#import requests
+import requests
 from django.http import Http404
 from django.template import RequestContext, loader
 from django.forms import ModelForm
